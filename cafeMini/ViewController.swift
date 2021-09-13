a8bda93aea7f40be3e7b707fecf3a1f27c68270e
JayTucker2 <prosnipa1221@gmail.com>
2021-09-13 08:50:28 -0500

    Initial Commit

--- cafeMini/ViewController.swift
+++ cafeMini/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cartLabel: UILabel!
+    var cart : [String: Double] = [:]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    cart["Apple"] = 2.50
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
 }
 
